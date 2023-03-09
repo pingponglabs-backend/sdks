a8eac16349b218fdac04c743ae2bab6d5f4a635e
Ewan Valentine <ewan.valentine89@gmail.com>
2023-03-09 17:17:24 +0000

    feat: added factory method to expose both clients

--- pingpong-typescript-sdk/src/index.ts
+++ pingpong-typescript-sdk/src/index.ts
@@ -1,2 +1,19 @@
-export * from './lib/models/models';
-export * from './lib/deployments/deployments';
+import Deployments from './lib/deployments/deployments';
+import Models from './lib/models/models';
+
+const _X_MEDIAMAGIC_KEY = process.env['X_MEDIAMAGIC_KEY'];
+
+// OneUp SDK factory
+class OneUp {
+
+    public models: Models;
+
+    public deployments: Deployments;
+
+    constructor(apiKey = _X_MEDIAMAGIC_KEY) {
+        this.models = new Models(apiKey);
+        this.deployments = new Deployments(apiKey);
+    }
+}
+
+export default OneUp;
