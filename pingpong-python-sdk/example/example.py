452d4d9d25c8f81192624f8da06fcdf42f49e6eb
amanjain97 <amanjain5221@gmail.com>
2023-09-27 13:23:10 +0530

    sync and job status added to python SDK

--- pingpong-python-sdk/example/example.py
+++ pingpong-python-sdk/example/example.py
@@ -6,17 +6,19 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from pingpongsdk.deployments.model import CreateDeployment
 from pingpongsdk import PingPong
 
+
 if __name__ == '__main__':
     pingpong = PingPong()
     try:
         deployment = pingpong.deployments.create(deployment=CreateDeployment(
-            name="example-deployment",
-            model='844218fa-c5d0-4cee-90ce-0b42d226ac8d',
-            args={
-                'input': 'https://cdn.mediamagic.dev/media/eb341446-be53-11ed-b4a8-66139910f724.jpg',
-            }
+        name="Background Removal",
+        model="844218fa-c5d0-4cee-90ce-0b42d226ac8d",
+        args={              
+        "input": "https://cdn.mediamagic.dev/media/799f2adc-384e-11ed-8158-e679ed67c206.jpeg",
+            },
+        sync=True
         ))
 
-        print(deployment.job)
+        print(deployment)
     except Exception as e:
         print(e)
